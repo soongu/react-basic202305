a703cffd223d1225bbc54fcf9838e6a5d820c1d9
Soon9 (SoonGu Hong) <naong526@naver.com>
2023-05-23 11:25:15 +0900

    # 3. 리액트 훅 - useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import SayHello from './SayHello';
-import SubmitEvent from './component/chap01_event/SubmitEvent';
-import FoodList from './component/chap02_props/FoodList';
+import Counter from './component/chap03_state/Counter';
+import NickName from './component/chap03_state/NickName';
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
   // {} 안에서 함수호출문이나 변수참조를 할 수 있음.
   return (
     <>
-      <FoodList />
+      <Counter />
     </>
   );
 }
---
+++ src/component/chap03_state/NickName.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const NickName = props => {
+
+  // useState훅은 배열을 리턴하는데 
+  // 0번에는 상태변수값, 1번에는 상태변수값을 변경하는 setter함수가 들어있음
+  const [nickName, setNickName] = useState('김철수');
+//   const nickName = statement[0];
+//   const setNickName = statement[1];
+
+//   console.log('statement:', statement);
+
+//   let nickName = '김철수';
+
+  const changeNickName = e => {
+    console.log('changeNickName!! call!');
+
+    // nickName = '척척박사';
+    setNickName('척척박사');
+    console.log('nickName: ', nickName);
+  };
+
+  return (
+    <>
+        <h1>Hello ~~~ {nickName}</h1>
+        <button className='btn' onClick={changeNickName}>척척박사</button>
+        <button className='btn' onClick={() => setNickName('척척석사')}>척척석사</button>
+    </>
+  ) 
+}
+
+
+export default NickName
